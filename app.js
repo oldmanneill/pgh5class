2b45331e43891a7fc8ea76324544e8e3bdf6e329
John Neill <oldmanneill@gmail.com>
2018-03-04 17:00:05 +0000

    general troubleshooting for deploy 6

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express'),
     methodOverride = require("method-override");
 
 //mongoose.connect('mongodb://localhost/random_groups1');
-mongoose.connect('mongodb://oldmanneill:academy5pgh@ds255258.mlab.com:55258/random_groups');
+mongoose.connect('mongodb://oldman:hellsyeah@ds255258.mlab.com:55258/random_groups');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
